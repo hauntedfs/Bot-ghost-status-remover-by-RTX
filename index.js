e337702eb28823ce523b72f74c81b8e9946db5f0
hauntedfs <139843646+hauntedfs@users.noreply.github.com>
2024-03-19 12:02:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Streaming Revive","Watching Support"];
+const statusMessages = ["Streaming Rogue","Watching Scripts"];
 
 
 let currentIndex = 0;
