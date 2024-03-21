e68c63fbff4766a56fde0d5e7f8314aa7e81b45a
hauntedfs <139843646+hauntedfs@users.noreply.github.com>
2024-03-21 09:34:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Streaming Rogue","Watching Scripts"];
+const statusMessages = ["Streaming Roblox Studios","Watching Skyhood"];
 
 
 let currentIndex = 0;
