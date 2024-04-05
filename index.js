381e3f591612903772bcb8cbae628def8ad20a43
hauntedfs <139843646+hauntedfs@users.noreply.github.com>
2024-04-04 21:54:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["🔗 bleed.bot","🔗 discord.gg/bleed"];
 
 
 let currentIndex = 0;
